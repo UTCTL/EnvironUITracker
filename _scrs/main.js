9449ba5b76627770823a3e5123fbe47bbae88bae
Samuel Acuna <samueleishion@gmail.com>
2014-07-23 09:46:44 -0500

    Stylized Region and Base focus

--- _scrs/main.js
+++ _scrs/main.js
@@ -137,6 +137,7 @@ $(document).ready(function() {
 	hide_show_menu(); 
 	premake_svgs(); 
 	handle_user(); 
+	bake_pie(); 
 
 	// choose item from menu 
 	$('.usermenu li').on('click',function() { 
@@ -722,4 +723,60 @@ function get_map_color(ratio) {
 	else if(ratio<0.9) return get_color_in_between(ratio,COLORS_GREEN,COLORS_YELLOW);
 	else if(ratio<1) return get_color_in_between(ratio,COLORS_YELLOW,COLORS_RED);
     else return COLORS_RED;
+}
+
+
+function bake_pie() {
+	var w = 300,                        //width
+    	h = 300,                            //height
+    	r = 150,                            //radius
+    	color = ['#f5f5f5','#ebebeb','#e5e5e5','#dbdbdb','#d5d5d5']; //d3.scale.category20c();     //builtin range of colors
+
+    // data = [{"label":"one", "value":20}, 
+    //         {"label":"two", "value":50}, 
+    //         {"label":"three", "value":30}, 
+    //         {"label":"four", "value":50}, 
+    //         {"label":"five", "value":50}, 
+    //         {"label":"six", "value":30}];
+    data = [{"label":"two", "value":50}, 
+            {"label":"four", "value":50}, 
+            {"label":"five", "value":50}, 
+            {"label":"three", "value":30}, 
+            {"label":"six", "value":20}, 
+            {"label":"one", "value":30}];
+    
+    var vis = d3.select(".piechart")
+        .append("svg:svg")              //create the SVG element inside the <body>
+        .data([data])                   //associate our data with the document
+        .attr("width", w)           //set the width and height of our visualization (these will be attributes of the <svg> tag
+        .attr("height", h)
+        .append("svg:g")                //make a group to hold our pie chart
+        .attr("transform", "translate(" + r + "," + r + ")")    //move the center of the pie chart from 0, 0 to radius, radius
+
+    var arc = d3.svg.arc()              //this will create <path> elements for us using arc data
+        .outerRadius(r);
+
+    var pie = d3.layout.pie()           //this will create arc data for us given a list of values
+        .value(function(d) { return d.value; });    //we must tell it out to access the value of each element in our data array
+
+    var arcs = vis.selectAll("g.slice")     //this selects all <g> elements with class slice (there aren't any yet)
+        .data(pie)                          //associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties) 
+        .enter()                            //this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
+        .append("svg:g")                //create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
+        .attr("class", "slice");    //allow us to style things in the slices (like text)
+
+    arcs.append("svg:path")
+        .attr("fill", function(d, i) { return get_color_in_between(i/data.length,'#d5d5d5','#f5f5f5'); /*return color[i];*/ } ) //set the color for each slice to be chosen from the color function defined above
+        .attr("d", arc);                                    //this creates the actual SVG path using the associated data (pie) with the arc drawing function
+
+    arcs.append("svg:text")                                     //add a label to each slice
+        .attr("transform", function(d) {                    //set the label's origin to the center of the arc
+            //we have to make sure to set these before calling arc.centroid
+            d.innerRadius = r/2;
+            d.outerRadius = r;
+            return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+        })
+        .attr("text-anchor", "middle")                          //center the text on it's origin
+        .text(function(d, i) { return data[i].label; });        //get the label from our original data array
+
 }
