5c55a1644c930968cacb45582878d40cce4a32aa
Samuel Acuna <samueleishion@gmail.com>
2014-07-23 15:30:25 -0500

    added styles to login screen

--- _apps/User/views.php
+++ _apps/User/views.php
@@ -4,9 +4,11 @@ class View {
 	
 	public function login() {
 		?>
-<input type="text" name="uname" id="uname" placeholder="Username"><br>
-<input type="password" name="pword" id="pword" placeholder="Password"><br>
-<input type="button" id="login" value="Log in">
+<div class="loginbox">
+	<input type="text" name="uname" id="uname" placeholder="Username"><br>
+	<input type="password" name="pword" id="pword" placeholder="Password"><br>
+	<input type="button" id="login" value="Log in">
+</div> 
 		<?
 		$_SESSION['ENVlogged'] = false; 
 	} 
