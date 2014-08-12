06297e94b56ab46df68de5e9d4eb999834b48642
Samuel Acuna <samueleishion@gmail.com>
2014-08-12 14:26:56 -0500

    Parsing json session on the server side #7

--- models/model.Session.php
+++ models/model.Session.php
@@ -19,6 +19,8 @@ class Session {
 	public $session_id; 
 	public $accessdatetime; 
 	public $name; 
+	public $ipaddress; 
+	public $citystate; 
 	public $spent_funds; 
 	public $spent_polcap; 
 	public $last_level; 
@@ -52,6 +54,104 @@ class Session {
 		$this->load('id'); 
 	}
 
+	public function instantiateByString($str) {
+		$j = json_decode($str,true); 
+		$id = ''; 
+		for($j as $a => $b) { $id = $a; }
+
+		$this->id 			= $row['id']; 
+		$this->session_id 	= $id; 
+		$this->accessdatetime  = now(); 
+		$this->name 		= $j[$id]["meta"]["name"]; 
+		$this->classcode 	= new Classcode($dblink,intval($row["meta"]['classcodes_id'])); 
+		$this->ipaddress 	= $j[$id]["meta"]["ipaddr"]; 
+		$this->citystate 	= $j[$id]["meta"]["citystate"]; 
+		$this->spent_funds 	= $j[$id]["details"]["spent_funds"]; 
+		$this->spent_polcap = $j[$id]["details"]["spent_polcap"]; 
+		$this->last_level 	= $j[$id]["details"]["level"]; 
+		$this->playtime 	= $j[$id]["details"]["playtime"]; 
+		$this->status 		= $j[$id]["details"]["status"]; 
+		$this->completed 	= $j[$id]["details"]["completed"]; 
+
+		$c = ''; 
+		foreach($j[$id]["input"]["screen_usage"] as $a) {
+			foreach($a as $b) {
+				$c .= '['.$b[0].','.$b[1].'],'; 
+			}
+		}
+		$this->clicks = rtrim($c,','); 
+
+		$this->drag = $j[$id]["input"]["camera_movement"]["drag"]; 
+		$this->arrows = $j[$id]["input"]["camera_movement"]["arrows"]; 
+		$this->wasdkeys = $j[$id]["input"]["camera_movement"]["keys"]; 
+		$this->minimap = $j[$id]["input"] ["navigation"]["minimap"]; 
+		$this->numkeys = $j[$id]["input"] ["navigation"]["keys"]; 
+		$this->panel_time_open = $j[$id]["input"]["panel_time"]["open"]; 
+		$this->panel_time_close = $j[$id]["input"]["panel_time"]["closed"]; 
+		$this->panel_clicks_open = $j[$id]["input"]["panel_time"]["click_open"]; 
+		$this->panel_clicks_close = $j[$id]["input"]["panel_time"]["click_close"]; 
+
+		$c = '{'; 
+		foreach($j[$id]["regions"] as $r => $d) {
+			$c .= '"'.$r.'": {'; 
+			foreach($d as $k => $v) {
+
+				if($k=='name'||$k=='initials')
+					$c .= '"'.$k.'" : "'.$v.'", '; 
+				elseif($k=='scores') {
+
+					$s = '"'.$k.'":{'; 
+					foreach($v as $scoretype => $scorevalue) {
+						$s .= '"'.$scoretype.'" : ['; 
+						foreach($scorevalue as $svi => $svc) {
+							$s .= $svc.','; 
+						} 
+						$s = rtrim($s,',').'],'; 
+					}
+					$s = rtrim($s,',').'},'; 
+					$c .= $s; 
+
+				} elseif($k=='bases') {
+
+					$b = '"'.$k.'" : ['; 
+					foreach($v as $baseproperty => $baseinfo) {
+						$b .= '{'; 
+						foreach($baseinfo as $baseinfoproperty => $baseinfovalue) {
+							if($baseinfoproperty=="upgrades") {
+								$b .= '"'.$baseinfoproperty.'":['; 
+								foreach($baseinfovalue as $upgradeindex => $upgradevalue) {
+									$b .= '{'; 
+									foreach($upgradevalue as $ui => $uv) {
+										if($ui=="active") {
+											$b .= '"'.$ui.'" : '.((!$uv) ? 'false' : 'true').','; 
+										} else {
+											$b .= '"'.$ui.'" : "'.$uv.'",'; 
+										}
+									}
+									$b = rtrim($b,',').'},'; 
+								}
+								$b = rtrim($b,',').'],'; 
+							} elseif($baseinfoproperty=='active') {
+								$b .= '"'.$baseinfoproperty.'" : '.((!$baseinfovalue) ? '0' : '1').','; 
+							} else {
+								$b .= '"'.$baseinfoproperty.'" : "'.$baseinfovalue.'",'; 
+							}
+						}
+						$b = rtrim($b,',').'},'; 
+					}
+					$b = rtrim($b,',').']'; 
+					$c .= $b; 
+
+				} else 
+					$c .= '"'.$k.'" : '.$v.', '; 
+
+			}
+			$c .= '},'; 
+		}
+		$this->region_data = rtrim($c,',').'}'; 
+
+	} 
+
 	public function getId() { return $this->id; } 
 	private function setId($int) { $this->id = clean($int); }
 
@@ -64,6 +164,8 @@ class Session {
 			$this->session_id 	= $row['session_id']; 
 			$this->accessdatetime  = $row['accessdatetime']; 
 			$this->name 		= $row['name']; 
+			$this->ipaddress 	= $row['ipaddress']; 
+			$this->citystate 	= $row['citystate']; 
 			$this->spent_funds 	= $row['spent_funds']; 
 			$this->spent_polcap = $row['spent_polcap']; 
 			$this->last_level 	= $row['last_level']; 
@@ -84,10 +186,6 @@ class Session {
 			$this->classcode 	= new Classcode($dblink,$row['classcodes_id']); 
 		}
 	} 
-
-	public function process($str) {
-		$j = json_decode($str); 
-	}
 
 	public function save() {
 		return false; 
